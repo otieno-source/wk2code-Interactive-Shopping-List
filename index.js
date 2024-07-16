b31eda67538c562a5a82339bf63b9da7c963430e
collins ouma otieno <otiencollins0@gmail.com>
2024-07-16 12:17:51 +0300

    revised shopping-list

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-### wk2code-Interactive-Shopping-List
-
-## html part
-1.have create 2 sections in my page; input area and out put area
-<div id="form"></div> as the input area with button 'btn1' to list user-input in the out put section
-
-<div id="output"></div> this is the output section dispalying user-input inform of a list. it has a clear button 'btn2' to clear its content.
-Another button here is the 'mark as purchesed' (btn3). it not working at the moment. it function was to mark element when user decied to buy them.
-
-
-
-## javascript part
-# Maintain a JavaScript array to store shopping list items
+// Maintain a JavaScript array to store shopping list items
 const newlist = [];
 function arraylisting() {
   const userinput = document.getElementById('inpt').value;
@@ -21,7 +9,7 @@ function arraylisting() {
 const btn = document.getElementById('btn1');
 btn.addEventListener('click', arraylisting);
 
-# Attach event listeners to the "Add" button to capture user input and add items to the list.
+//Attach event listeners to the "Add" button to capture user input and add items to the list.
 function creatinglist() {
   let mylist = document.getElementById('list');
   for (let i = 0; i < newlist.length; ++i) {
@@ -32,7 +20,7 @@ function creatinglist() {
 }
 btn.addEventListener('click', creatinglist);
 
-# Attach event listeners to the list of items to allow users to mark them as purchased
+//Attach event listeners to the list of items to allow users to mark them as purchased
 function strikethrough() {
   let listItems = document.getElementsByTagName('li');
   Array.from(listItems).forEach(item => {
@@ -43,7 +31,7 @@ const btn3 = document.getElementById('btn3');
 btn3.addEventListener('click', strikethrough);
 
 
-# Attach an event listener to the "Clear List" button to remove all items from the list.
+//Attach an event listener to the "Clear List" button to remove all items from the list.
 function deleting(){
   document.querySelector('ul').innerHTML = "";
 }
